99c7426d5284e498d0e656d01176e8b49868a37f
codemonkey017 <101619048+codemonkey017@users.noreply.github.com>
2022-06-05 11:51:59 -0700

    Add files via upload

---
+++ url-shortening-api-master/api.js
@@ -0,0 +1,18 @@
+
+
+// fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+
+  
+  const searchbar = document.querySelector(".searchbar__input")
+  const submit = document.querySelector(".submit")
+ 
+submit.addEventListener("click", function(){
+   alert("hello")
+     console.log(searchbar.value)
+     
+    })
+ 
+ 
